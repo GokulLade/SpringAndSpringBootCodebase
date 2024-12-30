a4b96fd49bd1a8cdd4382c04c0fb20aba2c10740
Gokul <gokullade89@gmail.com>
2024-12-30 16:31:56 +0530

    First Spring Stand alone application

---
+++ A_IOCProjectDependencyInjectionXMLConfiguration/src/com/nt/main/DependencyInjectionTest.java
@@ -0,0 +1,35 @@
+package com.nt.main;
+
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import com.nt.sbeans.WishMessageGenerator;
+
+public class DependencyInjectionTest {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("DependencyInjectionTest.main(Start)");
+		
+		//create the IOC container
+		FileSystemXmlApplicationContext ctx= new FileSystemXmlApplicationContext("src/com/nt/cfgs/applicationContext.xml");
+		
+		//get Target spring bean class object ref from the IOC container
+		Object obj=ctx.getBean("wmg");
+		
+		//Type casting
+		WishMessageGenerator generator=(WishMessageGenerator)obj;
+		
+		//Invoke the business method
+		String result=generator.generateWishMessage("Gokul");
+		
+		//Printing the result
+		System.out.println(result);
+		
+		//closing the IOC container
+		ctx.close();
+		
+		System.out.println("DependencyInjectionTest.main(End)");
+
+	}
+
+}
---
+++ A_IOCProjectDependencyInjectionXMLConfiguration/src/com/nt/sbeans/WishMessageGenerator.java
@@ -0,0 +1,42 @@
+package com.nt.sbeans;
+
+import java.time.LocalTime;
+
+public class WishMessageGenerator {
+
+	private LocalTime lt; // HAS-A property
+
+	public WishMessageGenerator() 
+	{
+		System.out.println("WishMessageGenerator.WishMessageGenerator()");
+	}
+
+	// setter method for setter Injection
+	public void setLt(LocalTime lt) 
+	{
+		System.out.println("WishMessageGenerator.setLt()");
+		this.lt = lt;
+	}
+
+	//business method
+	public String generateWishMessage(String user) 
+	{
+		System.out.println("WishMessageGenerator.generateWishMessage()");
+
+		// get current hour of the day
+		int hour = lt.getHour();// In 24 hrs format
+		
+		if (hour < 12)
+			return "Good Morning:" + user;
+
+		else if (hour < 16)
+			return "Good Afternoon:" + user;
+
+		else if (hour < 20)
+			return "Good Evening:" + user;
+		
+		else
+			return "Good Night:" + user;
+	}
+
+}

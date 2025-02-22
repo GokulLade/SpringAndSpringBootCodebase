237425b4fd93ac789d8c435f57f7c60e1d7d1348
Gokul <gokullade89@gmail.com>
2025-02-22 15:13:57 +0530

    SpringBoot Runners

---
+++ SB12_MiniProjectProfiles-Runners/src/main/java/com/nt/SSpringBootProject04LayerenMini01.java
@@ -0,0 +1,15 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSpringBootProject04LayerenMini01{
+
+	public static void main(String[] args) throws Exception 
+	{
+		//Creating IOC container
+		SpringApplication.run(SSpringBootProject04LayerenMini01.class, args);
+	}
+
+}

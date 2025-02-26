a56a2ab859e4fe3203a5a9a9244d4a0aa26c79ed
Gokul <gokullade89@gmail.com>
2025-02-27 00:03:50 +0530

    SpringBoot DataJPA Project

---
+++ SBDJPA01_CrurdRepository/src/main/java/com/nt/Sbdjpa01CrurdRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbdjpa01CrurdRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbdjpa01CrurdRepositoryApplication.class, args);
+	}
+
+}

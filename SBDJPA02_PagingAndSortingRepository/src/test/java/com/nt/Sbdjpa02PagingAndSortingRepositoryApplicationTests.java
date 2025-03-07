e862e0b4cb94f66a0cc1f5ba044691e13bd95653
Gokul <gokullade89@gmail.com>
2025-03-07 15:06:39 +0530

    Pre-defined method of Paging and sorting

---
+++ SBDJPA02_PagingAndSortingRepository/src/test/java/com/nt/Sbdjpa02PagingAndSortingRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbdjpa02PagingAndSortingRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

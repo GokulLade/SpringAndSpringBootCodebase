50b5afd52e94c31a2de96ba5ba818f064e7277fd
Gokul <gokullade89@gmail.com>
2025-09-03 13:01:53 +0530

    Single row and Specific entity Query programs

--- SBDJPA05_CustomQueryMethods/src/main/java/com/nt/repository/IDoctorRepository.java
+++ SBDJPA05_CustomQueryMethods/src/main/java/com/nt/repository/IDoctorRepository.java
@@ -9,22 +9,43 @@ import com.nt.entity.Doctor;
 
 public interface IDoctorRepository extends JpaRepository<Doctor, Integer> {
 
-	@Query("From Doctor")
-	List<Doctor> showAllDoctor() throws Exception; // All Doctors
+	/* ----------------------- Getting all Entity Record --------------------------------------*/
+	@Query("From Doctor") // All Doctors
+	public List<Doctor> showAllDoctor() throws Exception; 
 	
-	@Query("From Doctor where qly in(?1,?2)")
-	List<Doctor> showDoctorByQly(String qly1, String qly2) throws Exception;
 	
-	@Query("From Doctor where qly in(:qly1,:qly2)")
-	List<Doctor> showDoctorByQlyPara(String qly1, String qly2) throws Exception;
+	/* ----------------------- Use of Different Parameter --------------------------------------*/
+	@Query("From Doctor where qly in(?1,?2)") // Ordinary Para
+	public List<Doctor> showDoctorByQly(String qly1, String qly2) throws Exception;
 	
-	/*
+	@Query("From Doctor where qly in(:qly1,:qly2)") // Named Para
+	public List<Doctor> showDoctorByQlyPara(String qly1, String qly2) throws Exception;
+	 
 	@Query("From Doctor where qly in(?1,:qly2)") //Valid
-	List<Doctor> showDoctorByQlyPara(String qly1, String qly2) throws Exception;
+	public List<Doctor> showDoctorByQlyPara1(String qly1, String qly2) throws Exception;
 	
-	@Query("From Doctor where qly in(:qly1,?2)") //InvalidValid
-	List<Doctor> showDoctorByQlyPara(String qly1, String qly2) throws Exception;
+//	@Query("From Doctor where qly in(:qly1,?2)") //InvalidValid
+//	public List<Doctor> showDoctorByQlyPara2(String qly1, String qly2) throws Exception;
 	
 	
-	*/
+	/* ----------------------- Getting all Entity Record with some specific column -----------------------------*/
+	@Query("from Doctor where income>=:startInc and income<=:endInc order by income DESC") //Bulk Entity query
+	public List<Doctor> showDoctorByIncome(Double startInc,Double endInc) throws Exception; 
+	
+	@Query("Select docid,docName,qly from Doctor where qly in(:qly1,:qly2)") //Bulk Scalar/Projection query operation
+	public List<Object[]> showDoctorDataByQly(String qly1,String qly2) throws Exception; 
+	
+	@Query("Select docName from Doctor where qly=:qly1") //Bulk Scalar query giving single column value
+	public List<String> showDoctorDataByQly(String qly1) throws Exception; 
+	
+	
+	/* ----------------------- Getting an Entity Record with Single row --------------------------------------*/
+	@Query(" from Doctor where docName=:dName") // Single row doctor Query
+	public Doctor getDoctorByName(String dName) throws Exception;
+	
+	@Query(" Select docid,docName, income from Doctor where where docName=:dName") //Single row scalar query [Multiple columns]
+	public Object getDoctorDataByName(String dName) throws Exception; 
+	
+	@Query("Select income from Doctor where docName=:dName")
+	public Double getDoctorIncomeByName(String dName) throws Exception;
 }
--- SBDJPA05_CustomQueryMethods/src/main/java/com/nt/runners/JPARepositoryRunner.java
+++ SBDJPA05_CustomQueryMethods/src/main/java/com/nt/runners/JPARepositoryRunner.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.nt.entity.Doctor;
 import com.nt.repository.IDoctorRepository;
 
 @Component
@@ -13,11 +14,37 @@ public class JPARepositoryRunner implements CommandLineRunner {
 	private IDoctorRepository docRepo;
 
 	@Override
-	public void run(String... args) throws Exception 
-	{
-//		docRepo.showAllDoctor().forEach(System.out::println);
-//		docRepo.showDoctorByQly("Me", "MBBS").forEach(System.out::println);
-		docRepo.showDoctorByQlyPara("Me", "MBBS").forEach(System.out::println);
+	public void run(String... args) throws Exception {
+		try {
+
+			Doctor doctor = docRepo.getDoctorByName("okul");
+			if (doctor == null)
+				System.out.println("Name not found!!");
+			else {
+				System.out.println(doctor + " ");
+			}
+
+			Object data = docRepo.getDoctorDataByName("Gokul");
+			if (data == null)
+				System.out.println("Name not found!!");
+			else {
+				Object[] row = (Object[]) data;
+				for (Object d : row) {
+					System.out.println(d + " ");
+				}
+			}
+
+			Double income = docRepo.getDoctorIncomeByName("Gokul");
+			if (income == null)
+				System.out.println("Name not found!!");
+			else {
+				System.out.println(income);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
